f1eca02f4229bd78ec51a6199e14f92042f176ba
codeapollo0104 <ashwini0401@yahoo.com>
2024-12-17 20:48:48 -0800

    added simple functions

--- myapp/mainapp/src/App.jsx
+++ myapp/mainapp/src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 
-
+// create a component, which prints name age and hobby values.
 function App() {
-  
-
+  function handleClick() {
+    console.log("Hello world")
+  }
+  const name = "John"
+  const firstname = "Frank"
+  const age = 35
+  const hobby = " traveling"
   return (
-
-
     <>
-     <h1> This is my header tag : Hello </h1>
+      <h1 onClick={handleClick}> This is my header tag : Hello </h1>
 
-     {/*INSIDE THE return this is how you insert a comment {} */}
-       
+      {/*INSIDE THE return this is how you insert a comment {} */}
+      <p>my name is : {name}</p>
+      <p>{firstname}</p>
+      <p>I'm {age} years old </p>
+      <p>I love {hobby} </p>
+
+
     </>
   )
 }
