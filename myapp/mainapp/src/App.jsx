ea32a56ffb5d0356e489d868b4f8c0efa63daeed
codeapollo0104 <ashwini0401@yahoo.com>
2024-12-17 20:28:41 -0800

    added comments

--- myapp/mainapp/src/App.jsx
+++ myapp/mainapp/src/App.jsx
@@ -5,8 +5,12 @@ function App() {
   
 
   return (
+
+
     <>
      <h1> This is my header tag : Hello </h1>
+
+     {/*INSIDE THE return this is how you insert a comment {} */}
        
     </>
   )
--- myapp/mainapp/src/main.jsx
+++ myapp/mainapp/src/main.jsx
@@ -1,3 +1,6 @@
+
+// main execution file.
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
